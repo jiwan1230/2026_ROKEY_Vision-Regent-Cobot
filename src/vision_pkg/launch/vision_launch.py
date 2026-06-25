4204578691b0626768bdbb165caff4e29128e500
jiwan <jiwan1230@naver.com>
2026-06-25 12:27:46 +0900

    vision_launch default setting change img to device

--- src/vision_pkg/launch/vision_launch.py
+++ src/vision_pkg/launch/vision_launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
 
     model_path_arg = DeclareLaunchArgument("model_path", default_value=default_model_path)
     image_dir_arg = DeclareLaunchArgument("image_dir", default_value=default_image_dir)
-    source_mode_arg = DeclareLaunchArgument("source_mode", default_value="image_dir")
+    source_mode_arg = DeclareLaunchArgument("source_mode", default_value="device")
     camera_index_arg = DeclareLaunchArgument("camera_index", default_value="4")
 
     return LaunchDescription(
